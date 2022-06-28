35b04a1fd699b9492f12b609b57094e4a434dc27
권기은 <kgo6625@sungkyul.ac.kr>
2022-06-28 02:58:16 +0000

    자료형

---
+++ DAY1/DATA_TYPE_EX7.py
@@ -0,0 +1,15 @@
+# 리스트에 문자열을 저장하고 출력
+# 리스트 [] 선언, append 함수(내용 추가) 활용
+# While문 내에서 입력 받기 : if 엔터( ‘ ‘ ) 누르면 정지
+# 리스트 컴프리핸션 전체 출력 : for문
+
+array = []
+
+while True:
+    a = input("문자열을 입력하세요 : ")
+    array.append(a)
+    if a == ' ':
+        array.pop(-1)
+        break
+    
+print([i for i in array])
