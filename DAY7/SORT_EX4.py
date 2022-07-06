dbed97471f63f31e1259957f12e569a118f52f4c
권기은 <kgo6625@sungkyul.ac.kr>
2022-07-06 02:50:38 +0000

    이진 탐색 - 원소 개수 구하기

--- DAY7/SORT_EX4.py
+++ DAY7/SORT_EX4.py
@@ -1,21 +1,17 @@
+N, X = map(int, input().split())
+
+N_list = list(map(int, input('오름차순으로 정렬된 수열을 입력하시오. : ').split()))
+
 from bisect import bisect_left, bisect_right
-
-n, k = map(int, input().split()) # N과 K를 입력 받기
-a = []
-
-for i in range(n):
-    a.append(int(input()))
-
 
 # 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
 def count_by_range(a, left_value, right_value):
-	right_index = bisect_right(a, right_value)
-	left_index = bisect_left(a, left_value)
-	return right_index - left_index
-    
-if k not in a:
-    print(-1)
-else:
-    # 값이 4인 데이터 개수 출력
-    print(count_by_range(a, k, k))
+   if left_value == right_value and left_value not in a:
+      print('-1')
+   else: 
+      right_index = bisect_right(a, right_value)
+      left_index = bisect_left(a, left_value)
+      return right_index - left_index
+   
 
+print(count_by_range(N_list, X, X))
