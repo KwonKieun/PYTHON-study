857188035d93ef38adc494cfbd81a79909d71843
권기은 <kgo6625@sungkyul.ac.kr>
2022-07-07 05:25:29 +0000

    투 포인터 - 수 더하기

---
+++ DAY9/TWO_PLUS_EX3.py
@@ -0,0 +1,23 @@
+# 투포인터
+# N개의 수로 된 수열 A[1], A[2], ….A[N]이 있다.
+# 이 수열의 i번째 수부터 J번째 수까지의 합이 M이 되는 경우의 수를 구하는 프로그램을 작성하시오.
+
+n, m = map(int, input().split()) # 정수 N, M을 입력 받기
+data = list(map(int, input().split()))
+
+
+count = 0
+interval_sum = 0
+end = 0
+
+# start를 차례대로 증가시키며 반복
+for start in range(n):
+    # end를 가능한 만큼 이동시키기
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+# 부분합이 m일 때 카운트 증가
+    if interval_sum == m:
+        count += 1
+    interval_sum -= data[start]
+print(count)
